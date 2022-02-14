34a4e57e5fc68d40c140414dc1f819525a4cdb1e
Kacper Korban <kacper.f.korban@gmail.com>
2022-02-14 23:47:39 +0100

    Somewhat working

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
-## sbt project compiled with Scala 3
+## Scala 3 Wordle compile
 
-### Usage
+A Scala 3 compiler plugin, that requires you to solve a Wordle in order for the compilation to finish.
 
-This is a normal sbt project. You can compile code with `sbt compile`, run it with `sbt run`, and `sbt console` will start a Scala 3 REPL.
+### Demo
 
-For more information on the sbt-dotty plugin, see the
-[scala3-example-project](https://github.com/scala/scala3-example-project/blob/main/README.md).
+![](./docs/wordle_compile.gif)
+
+### Running locally
+
+```
+sbt package
+```
+
+```
+scala3 -Xplugin:path_to_pluginjar.jar file.scala
+```
+
+Default jar location after `sbt package`: `location_of_wordle-compile/target/scala-3.1.1/wordle-compile_3-0.1.0-SNAPSHOT.jar`
+
+Obviously inspired by [Wordle](https://www.nytimes.com/games/wordle/index.html).
--- src/main/scala/dev/korban/Wordle.scala
+++ src/main/scala/dev/korban/Wordle.scala
@@ -1,7 +1,56 @@
 package dev.korban
 
-object Wordle {
-  def finishWordle(): Unit = {
+import scala.io.*
+import scala.io.StdIn.*
+import scala.sys.process.*
+import scala.util.Random
 
+object Wordle {
+  lazy val wordsFile = "words.txt"
+  lazy val words =
+    Source.fromInputStream(Wordle.getClass.getClassLoader.getResourceAsStream(wordsFile)).getLines().toSeq
+
+  def finishWordle(): Unit = {
+    given expected: String = words(Random.between(0, words.size))
+
+    if !doPlay() then finishWordle()
   }
+
+  private def doPlay(i: Int = 1)(using expected: String): Boolean = {
+    if i > 6 then
+      false
+    else
+      print(s"$i. ")
+      val guess = readLine()
+
+      if words.contains(guess) then
+        println(generateHint(expected, guess))
+        if guess == expected then
+          true
+        else
+          doPlay(i+1)
+      else
+        println("Invalid guess!")
+        doPlay(i)
+  }
+
+  val greenCode  = "\u001b[42m"
+  val yellowCode = "\u001b[48;5;220m"
+  val blackCode  = "\u001b[40m"
+  val resetCode  = "\u001b[0m"
+
+  private def generateHint(expected: String, guess: String): String = {
+    guess.zip(expected).map {
+      case (gC, eC) =>
+        val color =
+          if gC == eC then
+            greenCode
+          else if expected.contains(gC) then
+            yellowCode
+          else
+            blackCode
+        color + gC + resetCode
+    }.fold("")(_ + _)
+  }
+
 }
